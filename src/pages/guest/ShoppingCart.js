e1bebb6ac9f59e81f98f683216dbf6dd006a5246
trongtq <tronghy97@gmail.com>
2018-12-14 18:00:28 +0700

    cap nhat giao dien danh sach yeu thich

--- src/pages/guest/ShoppingCart.js
+++ src/pages/guest/ShoppingCart.js
@@ -7,8 +7,21 @@ class ShoppingCart extends Component {
 
     render() {
         return (
-            <div>
-                <h1>User card</h1>
+            <div id="shopping_cart">
+                <div class="container">
+                    <h2>Giỏ hàng của tôi</h2>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <h4>Danh sách sản phẩm</h4>
+
+                            <input type="number" min={0} placeholder="min" />
+
+                        </div>
+                        <div class="col-sm-4">
+                            <h4>Thông tin đơn hàng</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
