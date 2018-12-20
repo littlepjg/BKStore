24fdb17dc7034cd35aecc598ac670ae7c429f6cc
tieunt97 <tieunt.bk97@gmail.com>
2018-12-21 01:24:57 +0700

    update query revenue

--- src/models/commonModel.js
+++ src/models/commonModel.js
@@ -1,9 +1,18 @@
 const db = require('../common/knex');
 
 const getInfoDashboard = async () => {
-    const resultUser = await db('users').count('id as totalUser').first();
-    const resultPost = await db('posts').count('id as totalPost').first();
-    const resultRevenue = await db('sale_details').select(db.raw('sum(unit_price*amount) as revenue')).first();
+    const resultUser = await db('users')
+        .count('id as totalUser')
+        .first();
+    const resultPost = await db('posts')
+        .count('id as totalPost')
+        .first();
+    const resultRevenue = await db('sale_details')
+        .select(db.raw('sum(unit_price*amount) as revenue'))
+        .whereIn('sale_details.id', db('sale_bills')
+            .select('id')
+            .where({ status_order: 2 })
+        ).first();
     return {
         totalUser: resultUser.totalUser,
         totalPost: resultPost.totalPost,
