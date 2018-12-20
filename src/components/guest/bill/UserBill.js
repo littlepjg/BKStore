21e63289dce552ac7669285b62c225539f22dfd9
tieunt97 <tieunt.bk97@gmail.com>
2018-12-20 21:39:09 +0700

    update user bill

--- src/components/guest/bill/UserBill.js
+++ src/components/guest/bill/UserBill.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import BillProductItem from './BillProductItem';
 
+import { formatDate } from '../../../helpers/formatDate';
+import { formatNumber } from '../../../helpers/formatNumber';
+
 const Container = styled.div`
     border: 1px solid #f57224;
     margin: 10px 0;
@@ -20,27 +23,23 @@ class UserBill extends Component {
         return (
             <Container>
                 <div data-toggle="collapse" data-target={`#bill-detail-${bill.id}`}>
-                    <div class="row">
-                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                            <p>Khách hàng: </p>
-                            <p>Nhân viên giao hàng: </p>
-                            <p>Giá trị đơn hàng: </p>
+                    <div className="row">
+                        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <p>Khách hàng: {bill.customer_name}</p>
+                            <p>Nhân viên giao hàng: {bill.shiper_name}</p>
+                            <p>Giá trị đơn hàng: <span>{formatNumber(bill.bill_value)} VNĐ</span></p>
                         </div>
 
-                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                            <p>Ngày đặt</p>
-                            <p>Ngày giao</p>
+                        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <p>Ngày đặt: {formatDate(bill.book_date)}</p>
+                            <p>Ngày giao: {formatDate(bill.delivery_date)}</p>
                         </div>
                     </div>
                 </div>
 
-                <div id={`bill-detail-${bill.id}`} class="collapse bill-detail">
-                    <div class="row">
-                        <BillProductItem />
-                        <BillProductItem />
-                        <BillProductItem />
-                        <BillProductItem />
-                        <BillProductItem />
+                <div id={`bill-detail-${bill.id}`} className="collapse bill-detail">
+                    <div className="row">
+                        {bill.products.map(p => (<BillProductItem key={p.id} product={p} />))}
                     </div>
                 </div>
             </Container>
--- src/pages/guest/UserOrder.js
+++ src/pages/guest/UserOrder.js
@@ -1,20 +1,115 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import { connect } from 'react-redux';
+
 import UserBill from '../../components/guest/bill/UserBill';
+
+const ROOT_URL = 'http://localhost:5000';
 
 const Container = styled.div`
     margin-top: 20px;
     margin-bottom: 20px;
 `;
 
+const ContinueDiv = styled.div`
+    height: 239px;
+    width: 100%;
+    padding-top: 80px;
+    text-align: center;
+
+    a {
+        color: #f57224;
+        border: 1px solid #f57224;
+        border-radius: 2px;
+        text-align: center;
+        font-size: 12px;
+        font-weight: 500;
+        letter-spacing: .5px;
+        padding: 0 8px;
+        height: 32px;
+        line-height: 32px;
+        display: inline-block;
+        text-transform: uppercase;
+    }
+
+    > p {
+        font-size: 14px;
+        color: #757575;
+        margin-bottom: 27px;
+    }
+`;
+
 class UserOrder extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            bills: [],
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`${ROOT_URL}/user/bill`, {
+            params: {
+                user_id: this.props.user_id
+            }
+        }).then(response => {
+            const { success, error, bills } = response.data;
+            if (success) {
+                this.setState({
+                    bills
+                });
+            }
+        }).catch(err => {
+            console.log(err);
+        });
     }
 
     render() {
+        const { bills } = this.state;
+
+        const renderBillOrder = () => {
+            const billOrder = bills.filter(b => b.status_order === 1);
+            return (
+                <div role="tabpanel" className="tab-pane active" id="order">
+                    {billOrder.map(b1 => (<UserBill key={b1.id} bill={b1} />))}
+                    <ContinueDiv>
+                        {billOrder.length === 0 && <p>Danh sách đơn hàng trống</p>}
+                        <a href="/" className="btn">Tiếp tục mua sắm</a>
+                    </ContinueDiv>
+                </div>
+            );
+        }
+
+        const renderBillRecived = () => {
+            const billRecived = bills.filter(b => b.status_order === 2);
+            return (
+                <div role="tabpanel" className="tab-pane" id="order-recived">
+                    {billRecived.map(b1 => (<UserBill key={b1.id} bill={b1} />))}
+                    <ContinueDiv>
+                        {billRecived.length === 0 && <p>Danh sách đơn hàng trống</p>}
+                        <a href="/" className="btn">Tiếp tục mua sắm</a>
+                    </ContinueDiv>
+                </div>
+            );
+        }
+
+        const renderBillCacel = () => {
+            const billCancel = bills.filter(b => b.status_order === 3);
+            return (
+                <div role="tabpanel" className="tab-pane" id="order-cancel">
+                    {billCancel.map(b1 => (<UserBill key={b1.id} bill={b1} />))}
+                    <ContinueDiv>
+                        {billCancel.length === 0 && <p>Danh sách đơn hàng trống</p>}
+                        <a href="/" className="btn">Tiếp tục mua sắm</a>
+                    </ContinueDiv>
+                </div>
+            );
+        }
+
         return (
             <Container className="container" role="tabpanel">
+                <h2>Đơn hàng của tôi</h2>
                 <ul className="nav nav-tabs" role="tablist">
                     <li role="presentation" className="active">
                         <a href="#order" aria-controls="home" role="tab" data-toggle="tab">Đơn hàng của tôi</a>
@@ -28,23 +123,19 @@ class UserOrder extends Component {
                 </ul>
 
                 <div className="tab-content">
-                    <div role="tabpanel" className="tab-pane active" id="order">
-                        <UserBill bill={{ id: 1 }} />
-                        <UserBill bill={{ id: 2 }} />
-                    </div>
-                    <div role="tabpanel" className="tab-pane" id="order-recived">
-                        <UserBill bill={{ id: 3 }} />
-                        <UserBill bill={{ id: 4 }} />
-                        <UserBill bill={{ id: 5 }} />
-                    </div>
-                    <div role="tabpanel" className="tab-pane" id="order-cancel">
-                        <UserBill bill={{ id: 6 }} />
-                        <UserBill bill={{ id: 7 }} />
-                    </div>
+                    {renderBillOrder()}
+                    {renderBillRecived()}
+                    {renderBillCacel()}
                 </div>
             </Container>
         )
     }
 }
 
-export default UserOrder;
+function mapStateToProps(state) {
+    return {
+        user_id: state.auth.user.id
+    }
+}
+
+export default connect(mapStateToProps)(UserOrder);
